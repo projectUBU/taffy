3fc3d2647c462fb4b58857cebc98405bde44f57a
born <kriengkrai.yo.60@ubu.ac.th>
2021-06-07 00:59:29 +0700

    Added files and changes required for deployment to heroku

--- taffydating/settings.py
+++ taffydating/settings.py
@@ -13,13 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 # SECRET_KEY = os.environ['SECRET_KEY']
 
-
-# import os
-# # import django_heroku
-# # django_heroku.settigns(locals(), staticfiles=False)
-# # import dj_database_url
-# # DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,15 +90,14 @@ WSGI_APPLICATION = 'taffydating.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-import dj_database_url
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
