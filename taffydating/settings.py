b10068d8ebdbd38f8604f7dfe4d38a3b3f44548f
born <kriengkrai.yo.60@ubu.ac.th>
2021-06-07 01:40:37 +0700

    Added files and changes required for deployment to heroku

--- taffydating/settings.py
+++ taffydating/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'chat.middleware.ActiveUserMiddleware',
 ]
 
