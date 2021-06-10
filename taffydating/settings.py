c80370e5a156b7781119acef573facbe11619c3e
born <kriengkrai.yo.60@ubu.ac.th>
2021-06-10 21:14:39 +0700

    into github

--- taffydating/settings.py
+++ taffydating/settings.py
@@ -185,41 +185,7 @@ USER_LASTSEEN_TIMEOUT = 60 * 60 * 24 * 7
 
 
 # Debugging in heroku live
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
-}
 
-DEBUG_PROPAGATE_EXCEPTIONS = True
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 
 CLOUDINARY_STORAGE = {
@@ -229,7 +195,7 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
+# CLOUDINARY_URL=cloudinary://317735717153535:McC6eSHG5ZNsRk0cCHCEWi7kTxs@ubon-ratchathani-university
 
 # Heroku logs
 # heroku logs --tail
